1be079339469c0147fdf7b5776b39e91ecd69629
hahuaz <hahuaz@gmail.com>
2021-08-09 19:19:52 +0300

    init repo

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+      <v-footer app>
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>hi from index page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: ({ isMobile }) => (isMobile ? 'mobile' : 'default'),
+}
+</script>
+
+<style lang="scss" scoped></style>
